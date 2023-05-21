e40029cf9d88e9ad7c7ac40339310e31cc104e7b
maroufia <maroufidis19@hotmail.com>
2023-05-21 17:33:02 +0300

    New Activity(settings) implemented dark mode (Hopefully) (NEEDS TESTING!)

---
+++ app/src/main/java/com/example/rollplay/Settings.java
@@ -0,0 +1,57 @@
+package com.example.rollplay;
+
+
+import static com.example.rollplay.R.id.dark_light;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class Settings extends AppCompatActivity {
+
+    Switch dark_light;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().hide();
+
+        dark_light = findViewById(R.id.dark_light);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE = sharedPreferences.getBoolean("night", false );
+
+        if (nightMODE){
+            dark_light.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        dark_light.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (nightMODE){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night",false);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night",true);
+                }
+                editor.apply();
+            }
+        });
+    }
+
+}
