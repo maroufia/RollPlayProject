bfecd112a89555b59e1611052642814091aebb77
maroufia <maroufidis19@hotmail.com>
2023-05-29 21:37:37 +0300

    Dark Mode switch is fixed and Dark mode on other Activities is now dark mode.Waiting to for new activities to commit a new fix.

--- app/src/main/java/com/example/rollplay/Settings.java
+++ app/src/main/java/com/example/rollplay/Settings.java
@@ -1,6 +1,7 @@
 package com.example.rollplay;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -14,6 +15,10 @@ import java.util.Objects;
 public class Settings extends AppCompatActivity {
 
     SwitchCompat dark_light;
+    static  final  String PREF_NAME = "MyAppPrefs";
+    static  final  String DARK_MODE_KEY = "darkMode";
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
 
     @Override
@@ -21,18 +26,26 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_settings);
-
-
+        sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        boolean darkModeEnabled = sharedPreferences.getBoolean(DARK_MODE_KEY, false);
         dark_light = findViewById(R.id.dark_light);
+        dark_light.setChecked(darkModeEnabled);
+
+
         dark_light.setOnCheckedChangeListener((buttonView, isChecked) -> {
 
-            if (isChecked)
+            if (isChecked) {
 
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                editor.putBoolean(DARK_MODE_KEY, true);
 
-             else
+            }else {
 
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                editor.putBoolean(DARK_MODE_KEY, false);
+            }
+            editor.apply();
         });
 }
 
