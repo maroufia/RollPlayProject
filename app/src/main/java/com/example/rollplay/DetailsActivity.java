9de5c3d422b1a98b7f93ca86791b5ffd07c532a4
iakompor <iakompor@csd.auth.gr>
2023-06-18 16:14:27 +0300

    Added comments toe Details and History. Also made the Sort button change to Unsort after being pressed.

--- app/src/main/java/com/example/rollplay/DetailsActivity.java
+++ app/src/main/java/com/example/rollplay/DetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.rollplay;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,10 +12,16 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/*
+The Details activity is accessible through the app's main activity by pressing the Details button after rolling.
+It shows each roll separately for every type of dice. Also has the ability to sort/unsort the results
+for more clear viewing.
+ */
 public class DetailsActivity extends AppCompatActivity {
 
-    private boolean alternate_sort;
+    private boolean alternate_sort = false; //Whether or not the results need to be sorted or not
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -28,36 +35,53 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        Button back_button = findViewById(R.id.back_button);
+        Button back_button = findViewById(R.id.back_button); //The screen's back button
 
-        Button sort_button = findViewById(R.id.sort_button);
-
-        Bundle bundle = getIntent().getExtras();
+        Button sort_button = findViewById(R.id.sort_button); //On press sorts the results. If pressed again it arranges them
+                                                             //in the order they were rolled.
+        Bundle bundle = getIntent().getExtras(); //The results of the last roll
         if (bundle != null) {
-            Details_Str(bundle, false);
+            Details_Str(bundle, false); //Call to Details_Str to arrange the dice in the order they were rolled.
         }
 
+        //The back button, which when pressed returns control to the main activity.
         back_button.setOnClickListener(v -> {
             Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
             setResult(1, intent);
             finish();
         });
 
+        /*
+        The sort button which checks if the results are currently sorted or not and calls Details_Str to either
+        sort or rearrange them in the order they were rolled.
+         */
         sort_button.setOnClickListener(v -> {
             if (bundle != null) {
                 alternate_sort = !alternate_sort;
                 Details_Str(bundle, alternate_sort);
+                if (alternate_sort)
+                    sort_button.setText("Unsort");
+                else
+                    sort_button.setText("Sort");
             }
         });
     }
 
+    /*
+    Details_Str is the function responsible for the layout of the TextViews in which the dice are arranged.
+    It receives the Bundle b which is the dice rolled and their results, as well as the boolean variable
+    sort. When sort is true it sorts the results of each dice type in ascending order separately. When it is
+    set to false it instead rearranges them to the order they were rolled. Also the TextViews are arranged so
+    that each type of dice rolled has equal space dynamically.
+     */
     private void Details_Str(Bundle b, boolean sort){
-        StringBuilder details = new StringBuilder();
+        StringBuilder details = new StringBuilder(); // The string that holds all the rolls for each type of dice.
         TextView d4_det = findViewById(R.id.details_d4), d6_det = findViewById(R.id.details_d6),
                 d8_det = findViewById(R.id.details_d8), d10_det = findViewById(R.id.details_d10),
                 d12_det = findViewById(R.id.details_d12), d20_det = findViewById(R.id.details_d20);
 
         if (b != null) {
+            //Arraylists with the results of each separate type of dice rolled.
             ArrayList<Integer> d4_rolls = new ArrayList<>(b.getIntegerArrayList("d4"));
             ArrayList<Integer> d6_rolls = new ArrayList<>(b.getIntegerArrayList("d6"));
             ArrayList<Integer> d8_rolls = new ArrayList<>(b.getIntegerArrayList("d8"));
@@ -65,22 +89,25 @@ public class DetailsActivity extends AppCompatActivity {
             ArrayList<Integer> d12_rolls = new ArrayList<>(b.getIntegerArrayList("d12"));
             ArrayList<Integer> d20_rolls = new ArrayList<>(b.getIntegerArrayList("d20"));
 
+            //If sort is true then it sorts each of the Arraylists.
             if (sort){
                 d4_rolls.sort(null); d6_rolls.sort(null); d8_rolls.sort(null);
                 d10_rolls.sort(null); d12_rolls.sort(null); d20_rolls.sort(null);
             }
 
-            d4_det.setText("");
-            if (d4_rolls.size() > 0) {
+            d4_det.setText(""); //At first the textView is empty.
+            if (d4_rolls.size() > 0) { //Checks if this type of dice was rolled.
+                //Sets the proper weights for the View so that the Views are dynamically arranged.
                 d4_det.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 0, 1f));
                 details.append("D4: ");
                 for (int i = 0; i < d4_rolls.size(); i++)
-                    details.append(d4_rolls.get(i)).append(", ");
-                details.delete(details.length()-2, details.length()-1);
+                    details.append(d4_rolls.get(i)).append(", "); //Appends and spaces each result for this type of dice to the string.
+                details.delete(details.length()-2, details.length()-1); //Deletes the last ", " added by the loop.
                 d4_det.append(details);
-                details = new StringBuilder();
+                details = new StringBuilder(); //After setting the TextView the StringBuilder is rest for use on the next dice type.
             }
 
+            //The above process repeats 5 more times, once for each type of dice.
 
             d6_det.setText("");
             if (d6_rolls.size() > 0) {
--- app/src/main/java/com/example/rollplay/HistoryActivity.java
+++ app/src/main/java/com/example/rollplay/HistoryActivity.java
@@ -11,13 +11,19 @@ import androidx.appcompat.app.AppCompatDelegate;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/*
+The History activity which shows the last four rolls of this session. The one at the top being
+the most recent.
+ */
+
 public class HistoryActivity extends AppCompatActivity {
 
-        //Checks if Dark Mode or Light Mode is enabled and sets its theme accordingly.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
+        //Checks if Dark Mode or Light Mode is enabled and sets its theme accordingly.
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
             setTheme(R.style.DarkMode);
         else
@@ -26,17 +32,18 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+        //The four different TextViews for each of the four last rolls of the session.
         TextView history1 = findViewById(R.id.roll1);
         TextView history2 = findViewById(R.id.roll2);
         TextView history3 = findViewById(R.id.roll3);
         TextView history4 = findViewById(R.id.roll4);
 
-        final ArrayList<String> recentRolls;
-        final ArrayList<String> recentRollResults;
-        final ArrayList<String> recentModifiers;
+        final ArrayList<String> recentRolls; //Arraylist that holds the number and type of dice rolled.
+        final ArrayList<String> recentRollResults; //Arraylist that holds the results of each dice rolled.
+        final ArrayList<String> recentModifiers; //Arraylist that holds the flat modifiers of the last rolls.
         Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            recentRolls = bundle.getStringArrayList("Recent Rolls"); // or other values
+        if (bundle != null) { //Fills the above Arraylists with the contents of bundle.
+            recentRolls = bundle.getStringArrayList("Recent Rolls");
             recentRollResults = bundle.getStringArrayList("Recent Roll Results");
             recentModifiers = bundle.getStringArrayList("Recent Modifiers");
         }
@@ -46,19 +53,28 @@ public class HistoryActivity extends AppCompatActivity {
             recentModifiers = null;
         }
 
-        String history_text;
+        String history_text; //String that is used to fill in each TextView.
         if (recentRolls != null)
+            /*
+            Checks how many rolls have been rolled this session and fills the Views accordingly
+            with the topmost being the most recent roll.
+             */
             switch (recentRolls.size()) {
-                case 1:
-                    if (recentModifiers.get(0).equals("0")){
+                case 1: //There has been only one roll this session.
+                    //Checks whether or not the user has inputted any flat modifiers to layout accordingly.
+                    if (recentModifiers.get(0).equals("0")){ //If there are no modifiers.
                         history_text = recentRolls.get(0) + "\nResult: " + recentRollResults.get(0);
                         history1.setText(history_text);
-                    } else {
+                    } else { //If there are modifiers.
                         history_text = recentRolls.get(0)  + recentModifiers.get(0) + "\nResult: " + recentRollResults.get(0);
                         history1.setText(history_text);
                     }
                     break;
-                case 2:
+                    /*
+                    This process is repeated for two, three, and four or more rolls, setting the most recent roll as the
+                    top one.
+                     */
+                case 2: //There have been two rolls this session.
                     if (recentModifiers.get(1).equals("0")){
                         history_text = recentRolls.get(1) + "\nResult: " + recentRollResults.get(1);
                         history1.setText(history_text);
@@ -74,7 +90,7 @@ public class HistoryActivity extends AppCompatActivity {
                         history2.setText(history_text);
                     }
                     break;
-                case 3:
+                case 3: //There have been three rolls this session.
                     if (recentModifiers.get(2).equals("0")){
                         history_text = recentRolls.get(2) + "\nResult: " + recentRollResults.get(2);
                         history1.setText(history_text);
@@ -97,7 +113,8 @@ public class HistoryActivity extends AppCompatActivity {
                         history3.setText(history_text);
                     }
                     break;
-                case 4:
+                case 4: //There have been four or more rolls this session. If there are more than four then the user can only see the
+                        //four most recent rolls of the session.
                     if (recentModifiers.get(3).equals("0")){
                         history_text = recentRolls.get(3) + "\nResult: " + recentRollResults.get(3);
                         history1.setText(history_text);
@@ -130,8 +147,9 @@ public class HistoryActivity extends AppCompatActivity {
             }
 
 
-        Button back = findViewById(R.id.back);
+        Button back = findViewById(R.id.back); //This activity's back button.
 
+        //When pressed returns control to the Settings Activity.
         back.setOnClickListener(v -> {
             Intent intent = new Intent(HistoryActivity.this, Settings.class);
             setResult(1, intent);
